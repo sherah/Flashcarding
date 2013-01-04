9f1660b259683d74cc31ffc2626947591aeea17d
Sherah <sherah@sherah.org>
2013-01-04 11:27:38 -0800

    wip working

--- src/CardSetView.js
+++ src/CardSetView.js
@@ -1,1 +1,55 @@
 //CardSetView
+window.CardSetView = Backbone.View.extend({
+	tagName: 'ul',
+
+	cardSet: [1, 2, 3, 4, 5, 6, 7],
+
+	//TODO: somehow get the data into this template ???
+	// ACTUALLY WAIT DO I NEED THIS TO BE HERE?
+	template: _.template("<li></li>"),
+
+
+	initialize: function(){
+		this.collection.bind("change", this.handleQueueChange.bind(this));
+
+		var that = this;
+		this.collection.each(function(card){
+			card.on("change:queued", function(){
+				that.render();
+			});
+		});
+
+		this.render();
+	},
+
+	render: function(){
+		var that = this;
+
+		if(that.cardSet.length === 0){
+			that.$el.html("<li>Select a card deck to begin!</li>");
+		} else {
+		// // 	//TODO: hook this up to the card deck collection and get its name etc
+			that.$el.html("<p class='titles'>Deck: </p>");
+			this.subviews = this.cardSet.map(function(card){
+				return new CardView({model:card});
+			});
+
+			_.each(this.subviews, function(subview){
+				that.$el.append(subview.render());
+			});
+
+		};
+
+		return this.$el;
+	},
+
+	handleQueueChange: function(){
+		//when you click a card to skip it, it goes to the end of the queue.
+		this.queued = this.collection.queued();
+		this.render();
+	},
+
+	
+});
+
+
--- src/CardView.js
+++ src/CardView.js
@@ -1,1 +1,53 @@
 //card views
+var CardView = Backbone.View.extend({
+	tagName: "li",
+
+	events: {
+		//on front:
+		//click 'answer' to see answer (aka flip over)
+
+		//on back:
+		//click 'got it' to add to answered deck
+
+		//everywhere:
+		//click 'pass' to put at end of queue
+		"click .pass": "passCard",
+
+		//make an "answered" card disappear from the displayed deck
+		"click .answer": "answerCard",
+
+		//click 'edit' to edit
+		"edit input": "editCard"
+
+	},
+
+	template: _.template("asdf"),
+	// template: _.template($('.single-card').html()),
+
+	render: function(){
+		return this.$el.html(this.template(this.model.attributes));
+	},
+
+	passCard: function(){
+		//change the card's queued number so it goes to the end of the queue
+
+		//re-render
+		this.render();
+	},
+
+	answerCard: function(){
+		//remove the card from the overall view
+
+	},
+
+	editCard: function(event){
+		var target = event.target;
+		var theEdit = {};
+		theEdit[target.name] = target.value;
+		this.model.set(theEdit);
+
+	}
+
+	//TODO: event listener function
+
+});
